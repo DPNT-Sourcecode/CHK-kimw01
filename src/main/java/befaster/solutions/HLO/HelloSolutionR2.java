0dff0c1a6dc3e763c66bb0c361f08b294a7e5d21
sbx_user1060 <sbx_user1060@ip-10-129-123-14.eu-west-2.compute.internal>
2019-02-22 22:41:54 +0000

    Fri Feb 22 22:41:54 UTC 2019

---
+++ src/main/java/befaster/solutions/HLO/HelloSolutionR2.java
@@ -0,0 +1,9 @@
+package befaster.solutions.HLO;
+
+public class HelloSolutionR2 {
+    public String hello(String friendName) {
+        StringBuffer sb = new StringBuffer("Hello, ").append(friendName).append("!");
+        return sb.toString();
+    }
+}
+
