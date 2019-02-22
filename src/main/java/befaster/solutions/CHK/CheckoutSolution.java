dcd2092ca39d687e3bef3f6d75aaf3e627bea6e9
sbx_user1059 <sbx_user1059@ip-10-129-233-228.eu-west-2.compute.internal>
2019-02-22 23:24:24 +0000

    Fri Feb 22 23:24:24 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -5,7 +5,8 @@ import befaster.runner.SolutionNotImplementedException;
 public class CheckoutSolution {
     public Integer checkout(String skus) {
 
-        throw new SolutionNotImplementedException();
+        
     }
 }
+
 
