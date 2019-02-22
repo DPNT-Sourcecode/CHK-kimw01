a5ccf75c3d68c2c2f472fca1552cf4bccdadc194
sbx_user1060 <sbx_user1060@ip-10-129-123-14.eu-west-2.compute.internal>
2019-02-22 22:26:57 +0000

    Fri Feb 22 22:26:57 UTC 2019

--- src/test/java/befaster/solutions/SUM/SumSolutionTest.java
+++ src/test/java/befaster/solutions/SUM/SumSolutionTest.java
@@ -19,4 +19,10 @@ public class SumSolutionTest {
     public void compute_sum() {
         assertThat(sum.compute(1, 1), equalTo(2));
     }
+
+    @Test
+    public void compute_sum_two() {
+        assertThat(sum.compute(15, 10), equalTo(25));
+    }
 }
+
