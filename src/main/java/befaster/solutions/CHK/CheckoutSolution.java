74806a59f1aba51dc516fa834d3b100c505dd1a2
sbx_user1059 <sbx_user1059@ip-10-129-233-228.eu-west-2.compute.internal>
2019-02-22 23:57:24 +0000

    Fri Feb 22 23:57:24 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -12,4 +12,3 @@ public class CheckoutSolution {
         return -1;
     }
 }
-
--- src/test/java/befaster/solutions/CHK/CheckoutSolutionR1Test.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionR1Test.java
@@ -26,4 +26,3 @@ public class CheckoutSolutionR1Test {
         assertThat(checkout.checkout("DD"), equalTo(30));
     }
 }
-
