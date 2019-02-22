2f78a1d1e13f2ed363a2e10e277698161193f829
sbx_user1059 <sbx_user1059@ip-10-129-233-228.eu-west-2.compute.internal>
2019-02-22 23:12:24 +0000

    Fri Feb 22 23:12:24 UTC 2019

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionR1Test.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionR1Test.java
@@ -1,4 +1,22 @@
 package befaster.solutions.CHK;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class CheckoutSolutionR1Test {
+    private CheckoutSolution checkout;
+
+    @Before
+    public void setUp(){
+        checkout = new CheckoutSolution();
+    }
+
+    @Test
+    public void checkout(){
+        assertThat(checkout.checkout("3A"), equalTo(130));
+    }
 }
+
