0e3aaf6de70eabe4c4fe2a988a7eb1b701c97ae6
sbx_user1060 <sbx_user1060@ip-10-129-249-247.eu-west-2.compute.internal>
2019-02-23 11:09:46 +0000

    Sat Feb 23 11:09:46 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -8,7 +8,19 @@ public class CheckoutSolution {
         // special offers
         // A = 50, 3A for 130 -> 150 - 130 = 20, (20/150)x100 = 13.333333% decrease
         // B = 30, 2B for 45 --> 60 - 45 = 15, (15/60)x100 = 25% decrease
+        // steps:
+        // validate illegal input
+        if (skus.contains("A") | skus.contains("B") | skus.contains("C") | skus.contains("D")) {
+            // create a list of items to use Collections to manage the list of items
+            // identify the frequency of items with offer (A and B)
+            // calculate the frequency of items without offer
+            // obtain the total price by item
+            // calculate the total price of basket
 
+        }
         return -1;
+
+
     }
 }
+
