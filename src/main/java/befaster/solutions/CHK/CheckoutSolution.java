efb2c6de3679b7067da1c4530b300f718b8f2c4b
sbx_user1060 <sbx_user1060@ip-10-129-249-247.eu-west-2.compute.internal>
2019-02-23 11:18:46 +0000

    Sat Feb 23 11:18:46 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,6 +2,11 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
 
@@ -12,8 +17,17 @@ public class CheckoutSolution {
         // validate illegal input
         if (skus.contains("A") | skus.contains("B") | skus.contains("C") | skus.contains("D")) {
             // create a list of items to use Collections to manage the list of items
+            List<String> items = Stream.of(skus.split("")).collect(Collectors.toList());
             // identify the frequency of items with offer (A and B)
+            int freqA = Collections.frequency(items,"A");
+            int freqB = Collections.frequency(items, "B");
             // calculate the frequency of items without offer
+            int freqC = Collections.frequency(items, "C");
+            int freqD = Collections.frequency(items, "D");
+            // groups of number of items * price to calculate offer for A and B
+            int offersA = freqA/3 * 130;
+            int offersB = freqB/2 * 45;
+            // price for individual items A and B out of offer
             // obtain the total price by item
             // calculate the total price of basket
 
@@ -23,3 +37,4 @@ public class CheckoutSolution {
 
     }
 }
+
