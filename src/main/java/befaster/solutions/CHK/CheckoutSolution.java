460446aa8504ec2c8d6d2ad8bd211a418679ea88
sbx_user1060 <sbx_user1060@ip-10-129-249-247.eu-west-2.compute.internal>
2019-02-23 11:42:46 +0000

    Sat Feb 23 11:42:46 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -18,36 +18,34 @@ public class CheckoutSolution {
         // validate illegal input, if there is any illegal input return -1
         // create a list of items to use Collections to manage the list of items
         List<String> items = Stream.of(skus.split("")).collect(Collectors.toList());
-        items.
-        items.removeIf(item -> item.equals("A") | item.equals("B") | item.equals("C") | item.equals("D"));
-        if(items.size()>0){
+        List<String> clone = items.stream().collect(Collectors.toList());
+        clone.removeIf(item -> item.equals("A") | item.equals("B") | item.equals("C") | item.equals("D"));
+        if (clone.size() > 0) {
             return -1;
         }
-        if (skus.contains("A") | skus.contains("B") | skus.contains("C") | skus.contains("D")) {
 
-            // identify the frequency of items with offer (A and B)
-            int freqA = Collections.frequency(items,"A");
-            int freqB = Collections.frequency(items, "B");
-            // calculate the frequency of items without offer
-            int freqC = Collections.frequency(items, "C");
-            int freqD = Collections.frequency(items, "D");
-            // groups of number of items * price to calculate offer for A and B
-            int offersA = freqA/3 * 130;
-            int offersB = freqB/2 * 45;
-            // price for individual items A and B out of offer using mod remainder
-            int noOfferA = freqA%3 * 50;
-            int noOfferB = freqB%2 * 30;
-            // obtain the total price for C and D
-            int priceC = freqC * 20;
-            int priceD = freqD * 15;
-            // return the total price of basket
-            return (offersA + offersB) + (noOfferA + noOfferB) + (priceC + priceD);
-        }
-        return -1;
+        // identify the frequency of items with offer (A and B)
+        int freqA = Collections.frequency(items, "A");
+        int freqB = Collections.frequency(items, "B");
+        // calculate the frequency of items without offer
+        int freqC = Collections.frequency(items, "C");
+        int freqD = Collections.frequency(items, "D");
+        // groups of number of items * price to calculate offer for A and B
+        int offersA = freqA / 3 * 130;
+        int offersB = freqB / 2 * 45;
+        // price for individual items A and B out of offer using mod remainder
+        int noOfferA = freqA % 3 * 50;
+        int noOfferB = freqB % 2 * 30;
+        // obtain the total price for C and D
+        int priceC = freqC * 20;
+        int priceD = freqD * 15;
+        // return the total price of basket
+        return (offersA + offersB) + (noOfferA + noOfferB) + (priceC + priceD);
 
 
     }
 }
+
 
 
 
