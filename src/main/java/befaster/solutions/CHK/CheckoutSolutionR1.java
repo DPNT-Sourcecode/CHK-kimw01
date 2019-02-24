89aae47288c3580e7665a1e9ff3e1a60ff5bd1de
sbx_user1059 <sbx_user1059@ip-10-128-97-110.eu-west-2.compute.internal>
2019-02-24 13:09:24 +0000

    Sun Feb 24 13:09:24 UTC 2019

---
+++ src/main/java/befaster/solutions/CHK/CheckoutSolutionR1.java
@@ -0,0 +1,50 @@
+package befaster.solutions.CHK;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CheckoutSolutionR1 {
+    public Integer checkout(String skus) {
+
+        // special offers
+        // A = 50, 3A for 130
+        // B = 30, 2B for 45
+        // C = 20, D = 15
+        // steps:
+
+        // basic validations: if object null, return -1, if the basket if empty return 0
+        if (skus == null) {
+            return -1;
+        }else if(skus.length()<=0){
+            return 0;
+        }
+
+        // create a list of items to use Collections to manage the list of items
+        List<String> items = Stream.of(skus.split("")).collect(Collectors.toList());
+
+        // special validation for illegal inputs, illegal items
+        // validate illegal entries cloning the list of items
+        List<String> clone = items.stream().collect(Collectors.toList());
+        // validate illegal input, if there is any illegal input return -1
+        clone.removeIf(item -> item.equals("A") | item.equals("B") | item.equals("C") | item.equals("D"));
+        // if the size of list is greater than 0 this means that the list contains illegal items or references
+        if (clone.size() > 0) {
+            return -1;
+        }
+
+        // price of A with offer
+        int priceItemsA = ((Collections.frequency(items, "A") / 3) * 130) + ((Collections.frequency(items, "A") % 3) * 50);
+        // price of B with offer
+        int priceItemsB = ((Collections.frequency(items, "B") / 2) * 45) + ((Collections.frequency(items, "B") % 2) * 30);
+        // price of C
+        int priceC = Collections.frequency(items, "C") * 20;
+        // price of D
+        int priceD = Collections.frequency(items, "D") * 15;
+        // return the total price of basket
+        return priceItemsA + priceItemsB + priceC + priceD;
+
+
+    }
+}
---
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionR2Test.java
@@ -0,0 +1,78 @@
+package befaster.solutions.CHK;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CheckoutSolutionR1Test {
+    private CheckoutSolution checkout;
+
+    @Before
+    public void setUp(){
+        checkout = new CheckoutSolution();
+    }
+
+    // offers 3A = 130, 2B = 45
+    // prices A = 50, B = 30, C = 20, D = 15
+    // no decimals, only integers
+
+    @Test
+    public void simpleItemATest(){
+        assertThat(checkout.checkout("AAA"), equalTo(130));
+        assertThat(checkout.checkout("AAAAAA"), equalTo(260));
+        assertThat(checkout.checkout("AAAAA"), equalTo(230));
+    }
+
+    @Test
+    public void simpleItemBTest(){
+        assertThat(checkout.checkout("BB"), equalTo(45));
+        assertThat(checkout.checkout("BBBB"), equalTo(90));
+        assertThat(checkout.checkout("BBB"), equalTo(75));
+    }
+
+    @Test
+    public void simpleItemCTest(){
+        assertThat(checkout.checkout("CCC"), equalTo(60));
+    }
+
+    @Test
+    public void simpleItemDTest(){
+        assertThat(checkout.checkout("DD"), equalTo(30));
+    }
+
+    @Test
+    public void combinationItemsTest(){
+        assertThat(checkout.checkout("ADADABCCDDBBBAC"), equalTo(390));
+    }
+
+    @Test
+    public void simpleIllegalInputTest(){
+        assertThat(checkout.checkout("F"), equalTo(-1));
+    }
+
+    /*
+    For empty basket we expected totalprice equals 0
+     */
+    @Test
+    public void voidBasketInputTest(){
+        assertThat(checkout.checkout(""), equalTo(0));
+    }
+
+    @Test
+    public void nullInputTest(){
+        assertThat(checkout.checkout(null), equalTo(-1));
+    }
+
+    @Test
+    public void combinationIllegalInputTest(){
+        assertThat(checkout.checkout("FMGGTT"), equalTo(-1));
+    }
+
+    @Test
+    public void combinationIllegalItemsAndCorrectItems(){
+        // calculate only correct Items, ignore illegal items
+        assertThat(checkout.checkout("FABBACCDDA"),equalTo(-1));
+    }
+}
