9c063bd2010d1170a68f6748fc641b1766838454
sbx_user1059 <sbx_user1059@ip-10-129-145-215.eu-west-2.compute.internal>
2019-02-25 20:30:29 +0000

    Mon Feb 25 20:30:29 UTC 2019

--- src/main/java/befaster/solutions/CHK/utility/ProductFactory.java
+++ src/main/java/befaster/solutions/CHK/utility/ProductFactory.java
@@ -14,62 +14,64 @@ public class ProductFactory implements ProductFactoryMethod {
         List<CombinationOffer> combinationOffers = new ArrayList<CombinationOffer>();
         switch (productRef) {
             case A:
+
                 return new Product(productRef, 50, offers, specialOffers, combinationOffers);
             case B:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 30, offers, specialOffers, combinationOffers);
             case C:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 20, offers, specialOffers, combinationOffers);
             case D:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 15, offers, specialOffers, combinationOffers);
             case E:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 40, offers, specialOffers, combinationOffers);
             case F:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 10, offers, specialOffers, combinationOffers);
             case G:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 20, offers, specialOffers, combinationOffers);
             case H:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 10, offers, specialOffers, combinationOffers);
             case I:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 35, offers, specialOffers, combinationOffers);
             case J:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 60, offers, specialOffers, combinationOffers);
             case K:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 70, offers, specialOffers, combinationOffers);
             case L:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 90, offers, specialOffers, combinationOffers);
             case M:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 15, offers, specialOffers, combinationOffers);
             case N:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 40, offers, specialOffers, combinationOffers);
             case O:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 10, offers, specialOffers, combinationOffers);
             case P:
                 return new Product(productRef, 50, offers, specialOffers, combinationOffers);
             case Q:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 30, offers, specialOffers, combinationOffers);
             case R:
                 return new Product(productRef, 50, offers, specialOffers, combinationOffers);
             case S:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 20, offers, specialOffers, combinationOffers);
             case T:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 20, offers, specialOffers, combinationOffers);
             case U:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 40, offers, specialOffers, combinationOffers);
             case V:
                 return new Product(productRef, 50, offers, specialOffers, combinationOffers);
             case W:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 20, offers, specialOffers, combinationOffers);
             case X:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 17, offers, specialOffers, combinationOffers);
             case Y:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 20, offers, specialOffers, combinationOffers);
             case Z:
-                return new Product(productRef, 50, offers, specialOffers, combinationOffers);
+                return new Product(productRef, 21, offers, specialOffers, combinationOffers);
             default:
                 return null;
         }
     }
 }
+
 
 
 
