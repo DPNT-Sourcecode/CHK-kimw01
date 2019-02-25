aadae34a3a8c575b0270073784d0b31100002eac
sbx_user1059 <sbx_user1059@ip-10-129-145-215.eu-west-2.compute.internal>
2019-02-25 19:30:29 +0000

    Mon Feb 25 19:30:29 UTC 2019

--- src/main/java/befaster/solutions/CHK/utility/Product.java
+++ src/main/java/befaster/solutions/CHK/utility/Product.java
@@ -1,4 +1,36 @@
 package befaster.solutions.CHK.utility;
 
-public class Product {
+public abstract class Product {
+
+    private Integer price;
+    private boolean haveOffer;
+    private boolean haveSpecialOffer;
+    private boolean haveCombinationOffer;
+
+    public Product(final Integer price, final boolean haveOffer, final boolean haveSpecialOffer, final boolean haveCombinationOffer){
+        this.price = price;
+        this.haveOffer = haveOffer;
+        this.haveSpecialOffer = haveSpecialOffer;
+        this.haveCombinationOffer = haveCombinationOffer;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public boolean isHaveOffer() {
+        return haveOffer;
+    }
+
+    public boolean isHaveSpecialOffer() {
+        return haveSpecialOffer;
+    }
+
+    public boolean isHaveCombinationOffer() {
+        return haveCombinationOffer;
+    }
+
+    // abstract methods of operations
+    // TODO
 }
+
